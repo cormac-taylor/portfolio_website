5dac1d5ca63a723666d80f90536e4fa309c42012
cormactaylor <cormacmtaylor@gmail.com>
2024-12-22 18:33:42 -0500

    progress on home page; stopping for dinner'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,20 @@ import "./styles/Home.css";
 function Home() {
   return (
     <>
-      <main className="page">
-        <h1>Home</h1>
-        <p>To be completed...</p>
-      </main>
+      <section className="full_screen_section">
+        <div id="landing_container">
+          <div>
+            <h1 id="landing_title">Cormac Taylor.</h1>
+            <p id="landing_description">
+              Highly curious touble shooter
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className="full_screen_divider"></div>
+      <section className="full_screen_section"></section>
+      <div className="full_screen_divider"></div>
+      <section className="full_screen_section"></section>
     </>
   );
 }
