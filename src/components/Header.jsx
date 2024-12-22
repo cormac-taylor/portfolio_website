b82c7962163be361038c33e13f1d440eeae99450
cormactaylor <cormacmtaylor@gmail.com>
2024-12-22 10:17:44 -0500

    header centered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,17 @@ import "./styles/Header.css";
 function Header() {
   return (
     <>
-      <div className="center">
-        <nav className="nav_container">
-          <a href="/" className="nav_link">
-            Home
-          </a>
-          <a href="/about" className="nav_link">
-            About
-          </a>
-          <a href="/blog" className="nav_link">
-            Blog
-          </a>
-        </nav>
-      </div>
+      <nav className="nav_container">
+        <a href="/" className="nav_link">
+          Home
+        </a>
+        <a href="/about" className="nav_link">
+          About
+        </a>
+        <a href="/blog" className="nav_link">
+          Blog
+        </a>
+      </nav>
     </>
   );
 }
