b94f1b8b1236da77a986cc03a0ea4126f5341edc
cormactaylor <cormacmtaylor@gmail.com>
2024-12-29 10:35:36 -0500

    css and context update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,17 +12,21 @@ function About() {
         <img src="/images/portrait_b&w.png" alt="Portrait of Cormac Taylor" />
       </div>
       <div className="center">
-      <div className="about_container right">
-        <h2>A {getAge(new Date(BIRTHDAY))} year old studying CS.</h2>
-        <p>I have a strong foundation in algorithms, machine learning, and web development.</p>
-        <p>
-          I am currently a course assistant for Sandeep Bhatt&apos;s Algorithmic Complexity at Stevens Institute of Technology.
-          I will be joining TD Securities&apos;s full time rotational program in July.
-        </p>
-        <Info
-          infoTitle="Understanding the Background"
-          infoDesc={"To be completed."}
-        />
+        <div className="about_container right">
+          <h2>A {getAge(new Date(BIRTHDAY))} year old studying CS.</h2>
+          <p>
+            I have a strong foundation in algorithms, machine learning, and web
+            development.
+          </p>
+          <p>
+            I am currently a course assistant for Sandeep Bhatt&apos;s
+            Algorithmic Complexity at Stevens Institute of Technology. I will be
+            joining TD Securities&apos;s full time rotational program in July.
+          </p>
+          <Info
+            infoTitle="Understanding the Background"
+            infoDesc={"To be completed."}
+          />
         </div>
       </div>
     </>
