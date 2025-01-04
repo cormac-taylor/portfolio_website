669e6a4917810d401f54802ea766263739f6befe
cormactaylor <cormacmtaylor@gmail.com>
2025-01-04 11:08:10 -0500

    making things more smooth

--- src/components/Circuit.jsx
+++ src/components/Circuit.jsx
@@ -1,5 +1,5 @@
 import "./styles/Circuit.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { euclideanDistance, getAngle } from "../utilities/index.js";
 
 const SPEED = 4;
@@ -7,102 +7,124 @@ const NUM_PARTICLES = 32;
 
 // https://codepen.io/WindOso/pen/PoXBYdb
 function Circuit() {
+  const [canPlay, setCanPlay] = useState(false);
   const canvasRef = useRef(null);
+
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const c = canvas.getContext("2d");
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    const p = [];
-
-    function getParticle(origin_x, origin_y, velocity) {
-      const partical = {};
-      partical.num_updates = 0;
-      partical.x = origin_x;
-      partical.y = origin_y;
-      partical.velocity = velocity;
-      partical.update = () => {
-        partical.num_updates++;
-        c.strokeStyle = "rgb(57, 152, 252)";
-        c.lineWidth = 1.5;
-        c.lineCap = "round";
-        c.beginPath();
-        c.moveTo(partical.x, partical.y);
-        partical.x += partical.velocity.x;
-        partical.y += partical.velocity.y;
-        c.lineTo(partical.x, partical.y);
-        c.stroke();
-
-        // change angle randomly
-        if (Math.random() < 0.128 && partical.num_updates % 8 === 0) {
-          let validAngles;
-          if (partical.velocity.cur_angle === partical.velocity.init_angle) {
-            validAngles = [
-              partical.velocity.init_angle - 1,
-              partical.velocity.init_angle + 1,
-            ];
-          } else {
-            validAngles = [partical.velocity.init_angle];
-          }
-
-          const i = Math.floor(Math.random() * validAngles.length);
-
-          const magnitude = euclideanDistance(
-            partical.velocity.x,
-            partical.velocity.y
-          );
-
-          const angle = getAngle(validAngles[i]);
-          partical.velocity.x = Math.cos(angle) * magnitude;
-          partical.velocity.y = Math.sin(angle) * magnitude;
-          partical.velocity.cur_angle = validAngles[i];
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setCanPlay(true);
+          observer.unobserve(entry.target);
         }
-      };
-      return partical;
+      },
+      { threshold: 0.2 }
+    );
+
+    if (canvasRef.current) {
+      observer.observe(canvasRef.current);
     }
 
-    // create particles
-    function pulse() {
-      for (var i = 0; i < NUM_PARTICLES; i++) {
-        p.push(
-          getParticle(
-            canvas.width / 2, // spawn x point
-            canvas.height / 2, // spawn y point
-            {
-              x: Math.cos(((i % 4) / 2) * Math.PI) * SPEED,
-              y: Math.sin(((i % 4) / 2) * Math.PI) * SPEED,
-              init_angle: (i % 4) * 2,
-              cur_angle: (i % 4) * 2,
-            }
-          )
-        );
-      }
-    }
-
-    // update cavas and remove particals once out of bounds
-    function gameMove() {
-      requestAnimationFrame(gameMove);
-      for (var i = 0; i < p.length; i++) {
-        p[i].update();
-        if (
-          p[i].x < 0 ||
-          p[i].x > canvas.width ||
-          p[i].y < 0 ||
-          p[i].y > canvas.height
-        ) {
-          p.splice(i, 1);
-        }
-      }
-    }
-
-    pulse();
-    gameMove();
+    return () => observer.disconnect();
   }, []);
+
+  useEffect(() => {
+    if (canPlay) {
+      const canvas = canvasRef.current;
+      const c = canvas.getContext("2d");
+
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      const p = [];
+
+      function getParticle(origin_x, origin_y, velocity) {
+        const partical = {};
+        partical.num_updates = 0;
+        partical.x = origin_x;
+        partical.y = origin_y;
+        partical.velocity = velocity;
+        partical.update = () => {
+          partical.num_updates++;
+          c.strokeStyle = "rgb(57, 152, 252)";
+          c.lineWidth = 1.5;
+          c.lineCap = "round";
+          c.beginPath();
+          c.moveTo(partical.x, partical.y);
+          partical.x += partical.velocity.x;
+          partical.y += partical.velocity.y;
+          c.lineTo(partical.x, partical.y);
+          c.stroke();
+
+          // change angle randomly
+          if (Math.random() < 0.128 && partical.num_updates % 8 === 0) {
+            let validAngles;
+            if (partical.velocity.cur_angle === partical.velocity.init_angle) {
+              validAngles = [
+                partical.velocity.init_angle - 1,
+                partical.velocity.init_angle + 1,
+              ];
+            } else {
+              validAngles = [partical.velocity.init_angle];
+            }
+
+            const i = Math.floor(Math.random() * validAngles.length);
+
+            const magnitude = euclideanDistance(
+              partical.velocity.x,
+              partical.velocity.y
+            );
+
+            const angle = getAngle(validAngles[i]);
+            partical.velocity.x = Math.cos(angle) * magnitude;
+            partical.velocity.y = Math.sin(angle) * magnitude;
+            partical.velocity.cur_angle = validAngles[i];
+          }
+        };
+        return partical;
+      }
+
+      // create particles
+      function pulse() {
+        for (var i = 0; i < NUM_PARTICLES; i++) {
+          p.push(
+            getParticle(
+              canvas.width / 2, // spawn x point
+              canvas.height / 2, // spawn y point
+              {
+                x: Math.cos(((i % 4) / 2) * Math.PI) * SPEED,
+                y: Math.sin(((i % 4) / 2) * Math.PI) * SPEED,
+                init_angle: (i % 4) * 2,
+                cur_angle: (i % 4) * 2,
+              }
+            )
+          );
+        }
+      }
+
+      // update cavas and remove particals once out of bounds
+      function gameMove() {
+        requestAnimationFrame(gameMove);
+        for (var i = 0; i < p.length; i++) {
+          p[i].update();
+          if (
+            p[i].x < 0 ||
+            p[i].x > canvas.width ||
+            p[i].y < 0 ||
+            p[i].y > canvas.height
+          ) {
+            p.splice(i, 1);
+          }
+        }
+      }
+
+      pulse();
+      gameMove();
+    }
+  }, [canPlay]);
   return (
     <>
-      <div id="circuitFrame"className="frame">
+      <div id="circuitFrame" className="frame">
         <canvas ref={canvasRef} className="canvas"></canvas>
       </div>
     </>
