1039e9644def95259b02015cdcb867eef7ecef85
cormactaylor <cormacmtaylor@gmail.com>
2025-01-04 18:28:18 -0500

    dummy info for projects and experiance

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./styles/Experiences.css";
 
 function Experiences() {
@@ -5,21 +6,52 @@ function Experiences() {
     <>
       <div id="experiences_div">
         <h2>Experience</h2>
-        <Experience />
-        <Experience />
-        <Experience />
-        <Experience />
+        <Experience
+          company="Stevens Institute of Technology"
+          title="Course Assistant"
+          location="Hoboken, NJ"
+          date="January, 2024 - Present"
+          desc="Algorithmic Complexity"
+        />
+        <Experience
+          company="JoStrong"
+          title="Lead Developer"
+          location="Hoboken, NJ"
+          date="August, 2024 - Present"
+          desc="Web Development"
+        />
+        <Experience
+          company="TD Securities"
+          title="Software Engineer"
+          location="New York, NY"
+          date="June, 2024 - August, 2024"
+          desc="Backend Engineering"
+        />
       </div>
     </>
   );
 }
 
-function Experience() {
+Experience.propTypes = {
+  company: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
+
+function Experience({ company, title, location, date, desc }) {
   return (
     <>
       <div className="experience">
-        <h3>Exp. Title</h3>
-        <p>Desc.</p>
+        <h3>{company}</h3>
+        <p>{title}</p>
+        <div>
+          <span>{location}</span>
+          <br />
+          <span>{date}</span>
+        </div>
+        <p>{desc}</p>
       </div>
     </>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import "./styles/Projects.css";
 
 function Projects() {
@@ -5,21 +6,61 @@ function Projects() {
     <>
       <div id="projects_div">
         <h2>Projects</h2>
-        <Project />
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          title="Proj_1"
+          desc="Desc."
+          youtube_url="https://www.youtube.com/@cormac-taylor"
+          github_url="https://github.com/cormac-taylor"
+        />
+        <Project
+          title="Proj_2"
+          desc="Desc."
+          youtube_url="https://www.youtube.com/@cormac-taylor"
+          github_url="https://github.com/cormac-taylor"
+        />
+        <Project
+          title="Proj_3"
+          desc="Desc."
+          youtube_url="https://www.youtube.com/@cormac-taylor"
+          github_url="https://github.com/cormac-taylor"
+        />
+        <Project
+          title="Proj_4"
+          desc="Desc."
+          youtube_url="https://www.youtube.com/@cormac-taylor"
+          github_url="https://github.com/cormac-taylor"
+        />
+        <Project
+          title="Proj_5"
+          desc="Desc."
+          youtube_url="https://www.youtube.com/@cormac-taylor"
+          github_url="https://github.com/cormac-taylor"
+        />
       </div>
     </>
   );
 }
+// https://www.youtube.com/@cormac-taylor
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  youtube_url: PropTypes.string.isRequired,
+  github_url: PropTypes.string.isRequired,
+};
 
-function Project() {
+function Project({ title, desc, youtube_url, github_url }) {
   return (
     <>
       <div className="project">
-        <h3>Proj. Title</h3>
-        <p>Desc.</p>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <div>
+          <a href={youtube_url} target="_blank">
+            YouTube
+          </a>
+          <br />
+          <a href={github_url} target="_blank">GitHub</a>
+        </div>
       </div>
     </>
   );
